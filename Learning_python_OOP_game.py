f1697a0cefbf8fad624591b8d4f89995d138979d
hodan121 <50772898+hodan121@users.noreply.github.com>
2019-10-21 19:00:17 +0300

    Update Learning_python_OOP_game.py
    Скрыты некоторые атрибуты и методы.
    Добавлен новый метод get_health
    Обновлен метод gaming для игры до победы

--- Learning_python_OOP_game.py
+++ Learning_python_OOP_game.py
@@ -1,63 +1,70 @@
 import random
 
 
-def initiative():
-    return random.random()
-
-
 class Person:
-    def __init__(self, race, damage, health, armor):
+    def __init__(self, race, _damage, _health, _armor):
         self.race = race
-        self.damage = damage
-        self.health = health
-        self.armor = armor
+        self._damage = _damage
+        self._health = _health
+        self._armor = _armor
 
     def get_atr(self):
         print(
-            f'У нас тут {self.race}, {self.health} ХП, тычка на {self.damage} с руки, армор поглощает {self.armor * 100}% урона')
+            f'У нас тут {self.race}, {self._health} ХП, тычка на {self._damage} с руки, армор поглощает {self._armor * 100}% урона')
+    
+    def get_health(self):
+        return self._health
 
-    def i_deal_damage(self):
-        return self.damage()
+    def _i_deal_damage(self):
+        return self._damage
 
-    def i_get_damage(self, enemy):
-        self.health -= enemy.i_deal_damage() * (1 - self.armor)
+    def _i_get_damage(self, enemy):
+        self._health -= enemy._i_deal_damage() * (1 - self._armor)
 
     def i_attack(self, enemy):
-        self.i_deal_damage()
-        enemy.i_get_damage(self)
+        self._i_deal_damage()
+        enemy._i_get_damage(self)
+
+    def initiative(self):
+        return random.random()
 
 
 class Player(Person):
-    def __init__(self, damage=100, health=300, armor=0.4):
+    def __init__(self, _damage=100, _health=300, _armor=0.4):
         self.race = 'Человек'
-        super().__init__(self.race, damage, health, armor)
+        super().__init__(self.race, _damage, _health, _armor)
 
 
 class Enemy(Person):
-    def __init__(self, damage=70, health=400, armor=0.2):
+    def __init__(self, _damage=70, _health=400, _armor=0.2):
         self.race = 'Андед'
-        super().__init__(self.race, damage, health, armor)
+        super().__init__(self.race, _damage, _health, _armor)
 
 
 class Game:
-    def __init__(self):
-        if initiative() > initiative():
-            self.attacker = Enemy()
-            self.defer = Player()
+    def __init__(self, player, enemy):
+        if enemy.initiative() > player.initiative():
+            self.attacker = enemy
+            self.defer = player
         else:
-            self.attacker = Player()
-            self.defer = Enemy()
+            self.attacker = player
+            self.defer = enemy
 
     def gaming(self):
-        self.attacker.i_attack(self.defer)
-        print(self.defer.health)
-
+        while True:
+            self.attacker.i_attack(self.defer)
+            if self.defer.get_health() <=0:
+                print(f'{self.attacker.race} выжил! У него осталось {self.attacker.get_health()} ХП')
+                break
+            else:
+                self.defer, self.attacker = self.attacker, self.defer
 
 human = Player()
 undead = Enemy()
+human.
 
 human.get_atr()
 undead.get_atr()
 
-game = Game()
+game = Game(human, undead)
 game.gaming()
